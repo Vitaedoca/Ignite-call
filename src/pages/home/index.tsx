9d4f3fdd5e2d163e02eb6859b8d05cf88de136d0
Vinicius Gabriel <viniciusgabrielpe@gmail.com>
2024-03-03 22:59:16 -0300

    primeiro ajuste

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,33 @@
+import { Heading, Text } from '@ignite-ui/react'
+// next/image automatiza o processo de otimização da imagem
+import Image from 'next/image'
+import { Container, Hero, Preview } from './styles'
+
+import previewImage from '../../assets/appInicial.png'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+          <Heading size="4xl">Agendamento Descomplicado</Heading>
+          <Text size="xl">
+            Conecte seu calendário e permita que as pessoas 
+            marquem agendamentos no seu tempo livre.
+          </Text>
+      </Hero>
+
+
+      <Preview>
+          <Image 
+            src={previewImage}
+            // A maior altura que eu quero que seja carregada
+            height={400}
+            // Diminui sempre para 80% então ajustamos para 100%
+            quality={100}
+            priority
+            alt='Calendario simbolizando aplicação em funcionamento'
+          />
+      </Preview>
+    </Container>
+  )
+}
